3d22aab28a916caf4c854a752bb05f6bfdc3f79b
Giuliano Espejo Mezzabotta <129322090+Giuliano-Espejo@users.noreply.github.com>
2024-05-13 19:57:26 -0300

    add endpoint getCategorias en sucursal controller

--- src/main/java/com/entidades/buenSabor/business/service/Imp/SucursalServiceImpl.java
+++ src/main/java/com/entidades/buenSabor/business/service/Imp/SucursalServiceImpl.java
@@ -2,9 +2,22 @@ package com.entidades.buenSabor.business.service.Imp;
 
 import com.entidades.buenSabor.business.service.Base.BaseServiceImp;
 import com.entidades.buenSabor.business.service.SucursalService;
+import com.entidades.buenSabor.domain.entities.Categoria;
 import com.entidades.buenSabor.domain.entities.Sucursal;
+import com.entidades.buenSabor.repositories.SucursalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SucursalServiceImpl extends BaseServiceImp<Sucursal,Long> implements SucursalService {
+
+    @Autowired
+    SucursalRepository sucursalRepository;
+
+    @Override
+    public List<Categoria> findCategoriasBySucursalId(Long idSucursal) {
+        return sucursalRepository.findWithCategoriasById(idSucursal).getCategorias().stream().toList();
+    }
 }
--- src/main/java/com/entidades/buenSabor/business/service/SucursalService.java
+++ src/main/java/com/entidades/buenSabor/business/service/SucursalService.java
@@ -1,8 +1,12 @@
 package com.entidades.buenSabor.business.service;
 
 import com.entidades.buenSabor.business.service.Base.BaseService;
+import com.entidades.buenSabor.domain.entities.Categoria;
 import com.entidades.buenSabor.domain.entities.Sucursal;
 
+import java.util.List;
+
 public interface SucursalService  extends BaseService<Sucursal, Long> {
+    public List<Categoria> findCategoriasBySucursalId(Long idSucursal);
 }
 
