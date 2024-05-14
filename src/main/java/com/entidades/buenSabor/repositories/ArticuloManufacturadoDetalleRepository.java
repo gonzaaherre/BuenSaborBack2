1941602ffb09d693aa333b31e902ea032a75f855
gonzaaherre <gonzaaherre@gmail.com>
2024-05-14 17:46:46 -0300

    fix de la referencia en articulo manufacturado detalle con insumo

--- src/main/java/com/entidades/buenSabor/repositories/ArticuloManufacturadoDetalleRepository.java
+++ src/main/java/com/entidades/buenSabor/repositories/ArticuloManufacturadoDetalleRepository.java
@@ -1,8 +1,17 @@
 package com.entidades.buenSabor.repositories;
 
+import com.entidades.buenSabor.domain.entities.ArticuloInsumo;
 import com.entidades.buenSabor.domain.entities.ArticuloManufacturadoDetalle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ArticuloManufacturadoDetalleRepository extends BaseRepository<ArticuloManufacturadoDetalle,Long> {
+
+    @Query("SELECT d FROM ArticuloManufacturadoDetalle d WHERE d.articuloInsumo = :insumo")
+    List<ArticuloManufacturadoDetalle> findByArticuloInsumo(@Param("insumo") ArticuloInsumo insumo);
 }
