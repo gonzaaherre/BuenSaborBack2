164a5c71eafc799bd68b9cfeb206af0449357075
Giuliano Espejo Mezzabotta <129322090+Giuliano-Espejo@users.noreply.github.com>
2024-05-16 10:55:27 -0300

    fix update articulo manufacturado

--- src/main/java/com/entidades/buenSabor/business/mapper/ArticuloManufacturadoMapper.java
+++ src/main/java/com/entidades/buenSabor/business/mapper/ArticuloManufacturadoMapper.java
@@ -8,11 +8,12 @@ import com.entidades.buenSabor.domain.entities.ArticuloManufacturado;
 import com.entidades.buenSabor.domain.entities.ArticuloManufacturadoDetalle;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 // En este caso, se utiliza el componente "spring" para la inyección de dependencias y se especifican
 // las clases de servicio y mappers que utiliza
-@Mapper(componentModel = "spring", uses = {ArticuloManufacturadoDetalleService.class, UnidadMedidaService.class, ImagenArticuloMapper.class})
+@Mapper(componentModel = "spring", uses = {ArticuloManufacturadoDetalleService.class, ArticuloManufacturadoDetalleMapper.class, UnidadMedidaService.class, ImagenArticuloMapper.class})
 public interface ArticuloManufacturadoMapper extends BaseMapper<ArticuloManufacturado, ArticuloManufacturadoDto, ArticuloManufacturadoCreateDto,ArticuloManufacturadoCreateDto> {
 
     // Esta es una instancia estática de la interfaz, que se utiliza para obtener una instancia del Mapper.
@@ -26,4 +27,10 @@ public interface ArticuloManufacturadoMapper extends BaseMapper<ArticuloManufact
     })
     // Este método define la transformación de un ArticuloManufacturadoCreateDto a una entidad ArticuloManufacturado.
     public ArticuloManufacturado toEntityCreate(ArticuloManufacturadoCreateDto source);
+
+    @Mappings({
+            @Mapping(source = "idsArticuloManufacturadoDetalles", target = "articuloManufacturadoDetalles", qualifiedByName = "getById"),
+            @Mapping(target = "unidadMedida", source = "idUnidadMedida",qualifiedByName = "getById")
+    })
+    public ArticuloManufacturado toUpdate(@MappingTarget ArticuloManufacturado entity, ArticuloManufacturadoCreateDto source);
 }
