cb5c489f15e7f5cd746686c6147180ede3949942
gonzaaherre <gonzaaherre@gmail.com>
2024-05-20 18:15:52 -0300

    imagenes para empresa

--- src/main/java/com/entidades/buenSabor/business/facade/EmpresaFacade.java
+++ src/main/java/com/entidades/buenSabor/business/facade/EmpresaFacade.java
@@ -6,8 +6,21 @@ import com.entidades.buenSabor.domain.dto.Empresa.EmpresaCreateDto;
 import com.entidades.buenSabor.domain.dto.Empresa.EmpresaDto;
 import com.entidades.buenSabor.domain.dto.Empresa.EmpresaEditDto;
 import com.entidades.buenSabor.domain.dto.Empresa.EmpresaLargeDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
 
 
 public interface EmpresaFacade extends BaseFacade<EmpresaDto, EmpresaCreateDto, EmpresaEditDto, Long> {
     EmpresaLargeDto getEmpresaSucursales(Long idEmpresa);
+
+    //Imagenes
+    // Método para obtener todas las imágenes almacenadas
+    ResponseEntity<List<Map<String, Object>>> getAllImagesByEmpresaId(Long id);
+    // Método para subir imágenes al sistema
+    ResponseEntity<String> uploadImages(MultipartFile[] files, Long id);
+    // Método para eliminar una imagen por su identificador público y UUID
+    ResponseEntity<String> deleteImage(String publicId, Long id);
 }
